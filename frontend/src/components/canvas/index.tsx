d4a3fe83a8615cd0ad659908b13e587ebf7de4d5
shenggwang <shenggwangg@gmail.com>
2023-04-07 23:36:48 +0100

    remove svg having canva only

--- frontend/src/components/canvas/index.tsx
+++ frontend/src/components/canvas/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 interface CanvasProps {
   width: number;
@@ -10,12 +11,14 @@ interface Rectangle {
   y: number;
   width: number;
   height: number;
+  id: string;
 }
 
 const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [rectangles, setRectangles] = useState<Rectangle[]>([]);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [reactangleId, setRectangleId] = useState('');
   const [startCoordinates, setStartCoordinates] = useState({ x: 0, y: 0 });
   const [endCoordinates, setEndCoordinates] = useState({ x: 0, y: 0 });
 
@@ -26,7 +29,7 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
       if (context) {
         context.clearRect(0, 0, canvas.width, canvas.height);
         rectangles.forEach((rectangle) => {
-          context.fillStyle = "rgba(255, 0, 0, 0.5)";
+          context.fillStyle = "red";
           context.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
         });
       }
@@ -34,6 +37,12 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
   }, [rectangles]);
 
   const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    let rect = rectangles.filter((rectangle) => (rectangle.x <= event.nativeEvent.offsetX && event.nativeEvent.offsetX <= rectangle.x + rectangle.width)
+    && (rectangle.y <= event.nativeEvent.offsetY && event.nativeEvent.offsetY <= rectangle.y + rectangle.height))
+    if (rect.length != 0) {
+      setRectangleId(rect[0].id)
+      return
+    }
     setIsDrawing(true);
     setStartCoordinates({ x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY });
   };
@@ -45,13 +54,20 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
   };
 
   const handleMouseUp = () => {
+    if (reactangleId != '') {
+      setRectangles(rectangles.filter((rectangle) => rectangle.id !== reactangleId));
+      setRectangleId('')
+      return
+    }
     setIsDrawing(false);
     const newRectangle: Rectangle = {
       x: Math.min(startCoordinates.x, endCoordinates.x),
       y: Math.min(startCoordinates.y, endCoordinates.y),
       width: Math.abs(endCoordinates.x - startCoordinates.x),
       height: Math.abs(endCoordinates.y - startCoordinates.y),
+      id: uuidv4(),
     };
+    console.log(newRectangle)
     setRectangles([...rectangles, newRectangle]);
   };
 
@@ -63,20 +79,7 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
-    >
-      <svg width={width} height={height}>
-        {rectangles.map((rectangle, index) => (
-          <rect
-            key={index}
-            x={rectangle.x}
-            y={rectangle.y}
-            width={rectangle.width}
-            height={rectangle.height}
-            fill="rgba(255, 0, 0, 0.5)"
-          />
-        ))}
-      </svg>
-    </canvas>
+    />
   );
 };
 
