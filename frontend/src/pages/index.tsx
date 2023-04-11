062d254a83167f865f99135ce18435756cacc985
shenggwang <shenggwangg@gmail.com>
2023-04-11 01:18:43 +0100

    chore

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -109,8 +109,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Colorize squares</title>
-        <meta name="description" content="Create and remove squares with color of your choise" />
+        <title>Draw Rectangles</title>
+        <meta name="description" content="Create and remove rectangles with color of your choice" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
